a26bdde8eed78e4451d0633240ed0847b7b9eecd
alexisljn <alexis.lejeune@gmx.fr>
2022-11-08 18:41:37 +0100

    add ethereum property in Window Interface

--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="react-scripts" />
+import {ExternalProvider} from "@ethersproject/providers"
+import {MetaMaskInpageProvider} from "@metamask/providers";
+
+declare global {
+    interface Window {
+        ethereum: ExternalProvider & MetaMaskInpageProvider
+    }
+}
+
