69630b34b1b044d96736ee0de901cffd50696f0b
alexisljn <alexis.lejeune@gmx.fr>
2022-11-13 12:46:50 +0100

    create Guard for admin panel access

--- frontend/src/components/AdminPanel.tsx
+++ frontend/src/components/AdminPanel.tsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
+import {UserContext} from "../App";
 
 function AdminPanel() {
-    return(
-        <h1>Admin Panel</h1>
+    // Recup statut du contrat
+    // Conditionner affichage et fonctionnement des features selon
+    // Changement statut :
+    // - Créer mapping chronologie des actions
+    // - Griser les invalides
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    const {isAdmin, votingStatus, changeVotingStatus} = useContext(UserContext);
+
+    useEffect(() => {
+        if (!isAdmin) {
+            window.location.href = '/not-found';
+            return;
+        }
+
+        setIsLoading(false);
+    }, []);
+
+
+    return (
+        isLoading
+            ? <div></div>
+            :
+                <div>
+                    <h2>Admin panel</h2>
+                </div>
+
     )
 }
 
